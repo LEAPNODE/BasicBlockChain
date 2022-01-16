b762d0d40e349ae8ed170ad42a3a69ddbd7fb5d1
LEAPNODE <brandon@solanaleap.com>
2022-01-16 00:28:05 -0600

    Testing IDE/Github/Git

---
+++ BlockchainTest.java
@@ -0,0 +1,8 @@
+public class BlockchainTest {
+ 
+    public static void main(String[] args) {
+
+        System.out.println("Basic Blockchain for Testing");
+    }
+  
+}
--- main.java
+++
@@ -1,7 +0,0 @@
-public class TestBlockChain {
- 
-    public static void main(String[] args) {
-      System.out.println("Basic Blockchain for Testing"); 
-    }
-  
-}
